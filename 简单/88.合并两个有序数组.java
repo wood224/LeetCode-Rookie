53c77631610dcbc1a001695583bb76092be88c6b
wood224 <92070171+wood224@users.noreply.github.com>
2022-04-23 11:05:17 +0800

    Add files via upload

---
+++ 简单/88.合并两个有序数组.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=java
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(j >= 0){
+            if(i < 0 || nums2[j] > nums1[i]){
+                nums1[k--] = nums2[j--];
+            }else{
+                nums1[k--] = nums1[i--];
+            }
+        }
+    }
+}
+// @lc code=end
+
