b79c052ec9888ecba230cdae2d9bc316a9556938
wood224 <92070171+wood224@users.noreply.github.com>
2022-04-23 11:35:23 +0800

    Add files via upload

---
+++ 简单/94.二叉树的中序遍历.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode t = root;
+        while(t != null || !stack.isEmpty()){
+            if(t != null){
+                stack.push(t);
+                t = t.left;
+            }else{
+                t = stack.pop();
+                list.add(t.val);
+                t = t.right;
+            }
+        }
+        return list;
+    }
+}
+// @lc code=end
+
